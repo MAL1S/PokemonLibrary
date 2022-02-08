b489b946d145a59c8d0a83a2a796fc0b73bc158b
MALIS <karbushev_ivan@mail.ru>
2022-02-08 20:29:37 +0800

    Add random pokemon displaying

--- app/src/main/java/com/example/pokemonlibrary/di/AppComponent.kt
+++ app/src/main/java/com/example/pokemonlibrary/di/AppComponent.kt
@@ -1,7 +1,9 @@
 package com.example.pokemonlibrary.di
 
 import android.content.Context
+import com.example.pokemonlibrary.presentation.favorite.FavoriteFragment
 import com.example.pokemonlibrary.presentation.main.MainActivity
+import com.example.pokemonlibrary.presentation.random.RandomFragment
 import com.example.pokemonlibrary.presentation.search.SearchFragment
 import dagger.BindsInstance
 import dagger.Component
@@ -18,4 +20,6 @@ interface AppComponent {
 
     fun inject(mainActivity: MainActivity)
     fun inject(searchFragment: SearchFragment)
+    fun inject(randomFragment: RandomFragment)
+    fun inject(favoriteFragment: FavoriteFragment)
 }
